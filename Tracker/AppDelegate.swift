c160cfadf40e7e88058b0323329fd862661c473b
KaMaDi <artkamadi@gmail.com>
2023-10-05 14:37:48 +0300

    сделал загрузочный экран и добавил все иконки и цвета сразу

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -1,33 +1,26 @@
-//
-//  AppDelegate.swift
-//  Tracker
-//
-//  Created by Jedi on 04.10.2023.
-//
-
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
- 
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfiguration.storyboard = nil
+        sceneConfiguration.sceneClass = UIWindowScene.self
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-       
-    }
-
-
 }
-
